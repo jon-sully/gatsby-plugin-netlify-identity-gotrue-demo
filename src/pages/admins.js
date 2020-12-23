b10b1131d974f9c8a4fbf05a1ea8e2b279cb70f3
Jon Sullivan <hey@jonsully.net>
2020-12-23 03:10:36 -0500

    dashboard changes; kitties

--- src/pages/admins.js
+++ src/pages/admins.js
@@ -22,6 +22,19 @@ const AdminsDashboard = () => {
           <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
             Admins Dashboard
           </h1>
+          <p className="px-4 mt-8 text-xl text-gray-700 sm:mt-8">
+            Congratulations! You've made it to 'admin' status. Yay! 😎
+          </p>
+          <p className="px-4 mt-8 text-xl text-gray-700 sm:mt-8">
+            There's not actually much here, but the <em>ONLY</em> way you
+            can see it is by logging in and having the `admin` role set for
+            your user! (Something you generally can't just click a button
+            for ordinarily) Here's a random kitty though!
+          </p>
+          <div className="flex justify-center align-center mt-6">
+            <img src="https://cataas.com/c" />
+
+          </div>
         </div>
       </div>
     </main>
