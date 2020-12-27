92d8deffa334b6febc58ec00aeef771dde0ba20c
Jon Sullivan <hey@jonsully.net>
2020-12-27 15:13:18 -0500

    docs

--- serverless/update-role/update-role.js
+++ serverless/update-role/update-role.js
@@ -2,6 +2,12 @@ const fetch = require('node-fetch')
 
 exports.handler = async (event, context) => {
 
+  // When you make a request to a Netlify Function using identity.authorizedFetch()
+  // client-side, context.clientContext becomes available in the function (here),
+  // and contains the user object, which is the verified, authorized user that is
+  // calling the function. This is validated with the JWT signing key used by GoTrue
+  // so if the .user object is there, we can know for certain that the calling client
+  // is indeed the authorized user. Yay!
   const { user, identity } = context.clientContext
   const { action, role } = JSON.parse(event.body)
 
@@ -20,6 +26,12 @@ exports.handler = async (event, context) => {
     }
   }
 
+  // Conversely to above, we can leverage the 'admin' abilities granted to Functions
+  // to make admin-level changes to users in our Netlify Identity instance. This is
+  // similar to logging into the Netlify UI and manually adding or removing a role.
+  //
+  // See this thread for more info on what the 'admin' context means:
+  // https://community.netlify.com/t/questions-about-netlify-identity-serverless-functions/20755/13?u=jonsully
   await fetch(userUrl, {
     method: 'PUT',
     headers: {
